ee93eece5c55e30e6859fad69993bd4d8ab0ec5f
richard <brayanrichard.vargas@alumnos.ulagos.cl>
2024-12-02 01:48:12 -0300

    sengunda entrega

--- src/Store/useCart.jsx
+++ src/Store/useCart.jsx
@@ -5,42 +5,39 @@ export const useCart = create((set, get) => ({
     products: products,
     cartItems: [],
     totalPrice: 0,
-    addToCart: (productId) => {
+
+    addToCart: (productId, quantity) => {
         set(() => {
             const product = get().products.find((item) => item.id === productId);
-            if (product) {
-                const cartItemFound = get().cartItems.find((carItem) => carItem.id === productId);
-
+            if (product && product.stock >= quantity) {
+                const cartItemFound = get().cartItems.find((cartItem) => cartItem.id === productId);
                 if (cartItemFound) {
-
                     return {
                         cartItems: get().cartItems.map((cartItem) =>
                             cartItem.id === productId
-                                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                                ? { ...cartItem, quantity: cartItem.quantity + quantity }
                                 : cartItem
                         ),
-                        totalPrice: get().totalPrice + product.price,
+                        totalPrice: get().totalPrice + product.price * quantity,
                     };
                 } else {
-
                     return {
-                        cartItems: [...get().cartItems, { ...product, quantity: 1 }],
-                        totalPrice: get().totalPrice + product.price,
+                        cartItems: [...get().cartItems, { ...product, quantity }],
+                        totalPrice: get().totalPrice + product.price * quantity,
                     };
                 }
             }
         });
     },
+
     removeFromCart: (productId) => {
         set((state) => {
             const itemIndex = state.cartItems.findIndex((cartItem) => cartItem.id === productId);
-
             if (itemIndex !== -1) {
                 const updatedCartItems = [...state.cartItems];
                 const itemToRemove = updatedCartItems[itemIndex];
 
                 if (itemToRemove.quantity > 1) {
-
                     updatedCartItems[itemIndex] = {
                         ...itemToRemove,
                         quantity: itemToRemove.quantity - 1,
@@ -50,7 +47,6 @@ export const useCart = create((set, get) => ({
                         totalPrice: state.totalPrice - itemToRemove.price,
                     };
                 } else {
-
                     updatedCartItems.splice(itemIndex, 1);
                     return {
                         cartItems: updatedCartItems,
@@ -58,8 +54,15 @@ export const useCart = create((set, get) => ({
                     };
                 }
             }
-
             return state;
+        });
+    },
+
+
+    clearCart: () => {
+        set({
+            cartItems: [],
+            totalPrice: 0,
         });
     },
 }));
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,24 +1,28 @@
 
-import { useCart } from "../../store/useCart";
-import styles from "../CartItem/styles.module.css";
-
 
 export const CartItem = ({ id, img, price, quantity }) => {
-    const { removeFromCart } = useCart();
+
 
     return (
-        <div className={styles.cartItem}>
-            <div className={styles.imgContainer}>
-                <img src={img} alt={id} />
-                <span>{quantity}</span>
+        <div className="justify-items-center flex items-center gap-6 p-4 border-b border-gray-700">
+
+            <div className="relative">
+                <img src={img} alt={id} className="w-12 h-12 object-cover rounded-md border border-gray-700" />
+                <span className="absolute top-0 right-0 w-6 h-6 flex items-center justify-center bg-red-600 text-white font-bold rounded-full -mt-2 -mr-2">
+                    {quantity}
+                </span>
             </div>
-            <div className={styles.textContainer}>
-                <span>Producto {id}</span>
-                <p>
-                    Precio por unidad: <b>${price}</b>
+
+
+            <div className="flex flex-col items-start">
+                <span className="font-bold text-white">Producto {id}</span>
+                <p className="text-xl text-gray-400 font-normal">
+                    Precio por unidad: <b className="text-white font-bold">${price}</b>
                 </p>
             </div>
-            <button onClick={() => removeFromCart(id)}>Remover</button>
+
+
+
         </div>
     );
 };
